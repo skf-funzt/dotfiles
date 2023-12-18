39527b1f4ce7bb2d23215024a16fb38a4a3053b0
Stephan Koglin-Fischer <stephan.koglin-fischer@funzt.dev>
2023-12-18 17:46:43 +0100

    fix: asdf-vm installed with the dcli

--- run_before_0001-install-dashlane-cli.sh
+++ run_before_0001-install-dashlane-cli.sh
@@ -5,7 +5,7 @@ if command -v yay &> /dev/null
 then
     echo "Yay is installed, installing dashlane-cli-git..."
     # Install aconfmgr for Arch configuration management
-    yay -Syu --needed --noconfirm asdf-vm dashlane-cli-git
+    yay -Syu --needed --noconfirm dashlane-cli-git
 else
     echo "Yay is not installed. Please install it and run this script again."
 fi
