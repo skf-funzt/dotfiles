445a486b103ad00db13d9e027b9ff0bb1b895ceb
Stephan Koglin-Fischer <stephan.koglin-fischer@funzt.dev>
2023-12-21 09:20:38 +0100

    fix: aconfmgr first run with artifical ignore scans the root chore: just saving the whole state with aconfmgr

--- run_before_0003-install-aconfmgr.sh
+++ run_before_0003-install-aconfmgr.sh
@@ -6,9 +6,6 @@ then
     echo "Yay is installed, installing aconfmgr-git..."
     # Install aconfmgr for Arch configuration management
     yay -Sy --needed --noconfirm aconfmgr-git
-    # Add an ignore root bath to aconfmgr
-    mkdir -p ~/.config/aconfmgr
-    echo "IgnorePath '/'" > ~/.config/aconfmgr/00-ignore-root-path.sh
     # Save the current system packages
     aconfmgr save
     # Move the file
