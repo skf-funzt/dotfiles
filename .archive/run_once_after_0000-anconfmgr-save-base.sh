e25e593d721157a7bf49579ed94a6d066779b3f1
Stephan Koglin-Fischer <stephan.koglin-fischer@funzt.dev>
2023-12-22 13:01:57 +0100

    fix: aconfmgr apply executed too late in the pipe

--- .archive/run_once_after_0000-anconfmgr-save-base.sh
+++ .archive/run_once_after_0000-anconfmgr-save-base.sh
@@ -2,4 +2,4 @@
 aconfmgr save
 
 # Move the file
-mv ~/./config/aconfmgr/99-unsorted.sh ~/./config/aconfmgr/01-base-packages.sh
+mv ./config/aconfmgr/99-unsorted.sh ./config/aconfmgr/01-base-packages.sh
