8e189ee92d4cd57c0de92cbe7e0269fe5e094dbb
Stephan Koglin-Fischer <stephan.koglin-fischer@funzt.dev>
2023-12-22 13:36:21 +0100

    fix: aconfmgr does not apply at the end

--- run_once_after_0000-anconfmgr-apply.sh
+++ run_once_after_0000-anconfmgr-apply.sh
@@ -3,7 +3,7 @@
 # Copy the file to .config/aconfmgr
 # cp ./01-base-packages.sh ./.config/01-base-packages.sh
 
-aconfmgr save
+aconfmgr apply
 
 # Move the file
 mv .config/aconfmgr/99-unsorted.sh .config/aconfmgr/01-base-packages.sh
